3143b8e617ba5d80ef0c88a3a6eeb57d44940430
Haley <haleyhfeng@gmail.com>
2020-12-02 01:36:31 -0800

    update html

--- daemon-serverless/handler.js
+++ daemon-serverless/handler.js
@@ -1,8 +1,8 @@
 const db = require('./db_connect');
 'use strict';
 
-
-module.exports.getAllTodos = (event, context, callback) => {
+// For Employees
+module.exports.getAllEMP = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
   db.getAll('project_requirements')
     .then(res => {
@@ -20,25 +20,7 @@ module.exports.getAllTodos = (event, context, callback) => {
     })
 };
 
-module.exports.createTodo = (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-  const data = JSON.parse(event.body);
-  db.insert('app_receive', data)
-    .then(res => {
-      callback(null,{
-        statusCode: 200,
-        body: "Todo Created!" + res
-      })
-    })
-    .catch(e => {
-      callback(null,{
-        statusCode: e.statusCode || 500,
-        body: "Could not create Todo " + e
-      })
-    })
-};
-
-module.exports.updateTodo = (event, context, callback) => {
+module.exports.updateEMP = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
   const data = JSON.parse(event.body);
   db.updateById('project_requirements', event.pathParameters.id, data)
@@ -55,6 +37,62 @@ module.exports.updateTodo = (event, context, callback) => {
       })
     }) 
 };
+
+
+// For Human Resource
+module.exports.getAllHR = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  db.getAll('app_receive')
+    .then(res => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(res)
+      })
+    })
+    .catch(e => {
+      console.log(e);
+      callback(null, {
+        statusCode: e.statusCode || 500,
+        body: 'Error: Could not find Todos: ' + e
+      })
+    })
+};
+
+module.exports.updateHR = (event, context, callback) => {
+  context.callbackWaitsorEmptyEventLoop = false;
+  const data = JSON.parse(event.body);
+  db.updateById('app_receive', event.pathParameters.id, data)
+    .then(res => {
+      callback(null,{
+        statusCode: 200,
+        body: "Todo Updated!" + res
+      })
+    })
+    .catch(e => {
+      callback(null,{
+        statusCode: e.statusCode || 500,
+        body: "Could not update Todo" + e
+      })
+    }) 
+};
+
+/*module.exports.createTodo = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  const data = JSON.parse(event.body);
+  db.insert('app_receive', data)
+    .then(res => {
+      callback(null,{
+        statusCode: 200,
+        body: "Todo Created!" + res
+      })
+    })
+    .catch(e => {
+      callback(null,{
+        statusCode: e.statusCode || 500,
+        body: "Could not create Todo " + e
+      })
+    })
+};*/
 
 
 
